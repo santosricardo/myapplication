14561aacb5ff4e2ef4c45c0427153189f602d17b
Ricardo <ricardoandrade@tjro.jus.br>
2022-09-12 12:05:01 -0400

    wip

--- myclub_website/events/models.py
+++ myclub_website/events/models.py
@@ -2,26 +2,24 @@ from operator import concat
 from statistics import mode
 from django.db import models
 from PIL import Image, ImageFont, ImageDraw
+from textwrap import wrap
 
 class Event(models.Model):
     description = models.TextField()
+    def __init__(self):
+        return self.description 
 
     def __str__(self):
         return self.description
 
-    def save_image():
-        #title_text = str(self.description)
-        title_text = "TIKPRAUtikpren1234567891012345678920123456789301JABEUAHEUAE"
-        new_text = ""
-        n = int(len(title_text))
-
-        for i in title_text:
-            new_text += i
-            if(int(len(new_text)) == 30)
-                new = '\n'.join(new_text)
-        print(new)
+    def save_image(self):
+        description = str(self.description)
+        #description = "No entanto, não podemos esquecer que a revolução dos costumes promove a alavancagem dos conhecimentos estratégicos para atingir a excelência."
+        new_description = ""
+        for i in wrap(description, width=25):
+            new_description += i + "\n"
         my_image = Image.open("/home/ricardo/projetos/estudos/myapplication/myclub_website/events/static/img/choris.png")
-        title_font = ImageFont.truetype('/home/ricardo/projetos/estudos/myapplication/myclub_website/events/static/fonts/US101.ttf', 40)
+        title_font = ImageFont.truetype('/home/ricardo/projetos/estudos/myapplication/myclub_website/events/static/fonts/US101.ttf', 50)
         image_editable = ImageDraw.Draw(my_image)
-        image_editable.multiline_text((20,20), title_text, (237, 230, 211), font=title_font)
+        image_editable.multiline_text((20,20), new_description, (237, 230, 211), font=title_font)
         my_image.save("/home/ricardo/projetos/estudos/myapplication/myclub_website/events/static/img/choris-1.png")
