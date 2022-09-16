b292956ecec6b3fe7d7edab102753228976c47e1
Ricardo <ricardoandrade@tjro.jus.br>
2022-09-16 14:06:47 -0400

    wip

--- myclub_website/events/models.py
+++ myclub_website/events/models.py
@@ -1,10 +1,12 @@
+#import os 
 from operator import concat
 from statistics import mode
 from django.db import models
 from PIL import Image, ImageFont, ImageDraw
 from textwrap import wrap
-#from myclub_website.events.views import generate
 
+#path = os.listdir("/home/ricardo/projetos/estudos/myapplication/myclub_website/events/static/")
+path = "/home/ricardo/projetos/estudos/myapplication/myclub_website/events/static/"
 class Event(models.Model):
     description = models.TextField()
     # def __init__(self):
@@ -19,11 +21,11 @@ class Event(models.Model):
             new_description += i + "\n"
         len_description = len(new_description)
         if len_description < 200:
-            my_image = Image.open("/home/ricardo/projetos/estudos/myapplication/myclub_website/events/static/img/choris.png")
-            title_font = ImageFont.truetype('/home/ricardo/projetos/estudos/myapplication/myclub_website/events/static/fonts/US101.ttf', 45)
+            my_image = Image.open(path + "img/choris.png")
+            title_font = ImageFont.truetype(path + "fonts/US101.ttf", 45)
             image_editable = ImageDraw.Draw(my_image)
             image_editable.multiline_text((20,20), new_description, (237, 230, 211), font=title_font)
-            my_image.save("/home/ricardo/projetos/estudos/myapplication/myclub_website/events/static/img/choris-quotes.png")
+            my_image.save(path + "img/choris-quotes.png")
         else:
             new_description = 'Sorry, your citation cannot be generated. There is a limit of 200 characters.'
         return new_description
